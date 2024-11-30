4c39f93e02693b6587a283e5c01e91ac261aa206
HanBeom98 <carry9691@gmail.com>
2024-11-30 08:02:32 +0000

    Initial commit

---
+++ src/test/java/com/spring/smaple/SmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.smaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
